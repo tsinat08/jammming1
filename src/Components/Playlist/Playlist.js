bbe4dcb99cda76934a748d79f6d66646454e1712
tsinat08 <tsinat.weldetnesae@gmail.com>
2020-02-23 18:07:35 -0500

    first commit

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './Playlist.css';
+import TrackList from'../TrackList/TrackList'
 
 class Playlist extends React.Component{
     render() {
         return(
             <div className="Playlist">
                 <input value="New Playlist"/>
-                <!-- Add a TrackList component -->
+                <TrackList/>
                 <button className="Playlist-save">SAVE TO SPOTIFY</button>
             </div>
         )
--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import 'Track.css';
+import './Track.css';
 
 class Track extends React.Component{
     render() {
