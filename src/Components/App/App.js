a72992eb73b8a9254994e8dacb22b0f6864796d2
tsinat08 <tsinat.weldetnesae@gmail.com>
2020-02-24 20:42:08 -0500

    Pass Down Search Result and Render Result List

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -5,6 +5,17 @@ import SearchResults from '../SearchResults/SearchResults';
 import Playlist from '../Playlist/Playlist';
 
 class App extends React.Component{
+    constructor (props){
+        super(props);
+
+        this.state={
+            searchResults: [{}],
+            playlistName: 'My playlist',
+            playlistTracks: []
+        };
+
+
+    }
   render() {
     return (
         <div>
@@ -12,7 +23,7 @@ class App extends React.Component{
           <div className="App">
             <SearchBar/>
             <div className="App-playlist">
-             <SearchResults/>
+             <SearchResults searchResults={this.state.searchResults}/>
              <Playlist/>
             </div>
           </div>
