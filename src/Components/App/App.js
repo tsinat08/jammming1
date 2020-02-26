4f1acc6cad24000908a39d1434c32ec739c4f470
tsinat08 <tsinat.weldetnesae@gmail.com>
2020-02-26 13:57:11 -0500

    Change the Name of a Playlist

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -4,6 +4,7 @@ import SearchBar from '../SearchBar/SearchBar';
 import SearchResults from '../SearchResults/SearchResults';
 import Playlist from '../Playlist/Playlist';
 
+
 class App extends React.Component{
     constructor (props){
         super(props);
@@ -13,7 +14,9 @@ class App extends React.Component{
                             {name: 'name2', artist: 'artist2', album: 'album2', id: 2},
                             {name: 'name3', artist: 'artist3', album: 'album3', id: 3}],
             playlistName: 'My playlist',
-            playlistTracks: [ ]
+            playlistTracks: [{name: 'name1', artist: 'artist1', album: 'album1', id: 1},
+        {name: 'name2', artist: 'artist2', album: 'album2', id: 2},
+        {name: 'name3', artist: 'artist3', album: 'album3', id: 3}]
         };
         this.addTrack = this.addTrack.bind(this);
         this.removeTrack=this.removeTrack.bind(this);
@@ -25,7 +28,6 @@ class App extends React.Component{
        if(tracks.find(savedTrack => savedTrack.id === track.id)) {
            return;
        }
-
        tracks.push(track)
        this.setState({playlistTracks : tracks})
    }
@@ -48,10 +50,10 @@ class App extends React.Component{
             <SearchBar/>
             <div className="App-playlist">
              <SearchResults searchResults={this.state.searchResults}
-                            onAdd={this.addTrack()}/>
+                            onAdd={this.addTrack}/>
              <Playlist playlistName={this.state.playlistName}
                        playlistTracks ={this.state.playlistTracks}
-                        onRemove={this.removeTrack()}
+                       onRemove={this.removeTrack}
                         onNameChange={this.updatePlaylistName   }/>
             </div>
           </div>
--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -2,20 +2,15 @@ import React from 'react';
 import './Track.css';
 
 class Track extends React.Component{
-    constructor(props) {
-        super(props);
-        this.addTrack=this.addTrack.bind(this);
-        this.removeTrack=this.removeTrack.bind(this);
+
+    renderAction(){
+        if(this.props.isRemoval){
+            return <button className="Track-action" >-</button>
+        }else {
+            return <button className="Track-action" >+</button>
+        }
     }
 
-
-    addTrack() {
-        this.props.onAdd(this.props.track);
-    }
-
-    removeTrack(){
-        this.props.onRemove(this.props.track)
-    }
 
     render() {
         return(
@@ -24,8 +19,7 @@ class Track extends React.Component{
                     <h3>{this.props.track.name}</h3>
                     <p>{this.props.track.artist} | {this.props.track.album}</p>
                 </div>
-                <button className="Track-action" onClick={this.addTrack} >+</button>
-                <button className="Track-action" onClick={this.removeTrack} >-</button>
+                {this.renderAction()}
             </div>
         )
     }
