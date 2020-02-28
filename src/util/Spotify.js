7b00e763aca91458e70ede0406348d27b28ea085
tsinat08 <tsinat.weldetnesae@gmail.com>
2020-02-27 21:40:48 -0500

    Implement Spotify Search Request

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -14,7 +14,7 @@ const Spotify={
 
         if(accessTokenMatch && expiresInMatch){
             accessToken =accessTokenMatch[1];
-            const expiresIN = Number(expiresInMatch[1]);
+            const expiresIn = Number(expiresInMatch[1]);
 
             window.setTimeout(() => accessToken = '', expiresIn * 1000);
             window.history.pushState('Access Token', null, '/');
@@ -25,7 +25,31 @@ const Spotify={
             window.location= accessUrl
         }
 
+    },
+
+    search(term){
+        const accessToken = Spotify.getAccessToken();
+        return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`,
+            {header: {
+                Authorization: `Bearer ${accessToken}`
+                }
+            }).then(response =>{
+                return response.json();
+            }).then(jsonResponse => {
+                if(!jsonResponse.tracks){
+                    return [];
+            }
+
+            return jsonResponse.tracks.items.map(track => ({
+                id: track.id,
+                name: track.name,
+                artists: track.artist[0].name,
+                album: track.album.name,
+                uri: track.uri
+            }));
+            });
     }
+
 
 }
 
